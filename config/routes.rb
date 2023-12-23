cf0c2b98bfb4b27dbe8c23bcadb72cccbf30ecbd
linuxpro3000 <83082486+aleksandar-code@users.noreply.github.com>
2023-12-23 14:03:51 +0100

    Create friends

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :friends
   resources :home
   devise_for :users
   resources :posts
--- db/migrate/20231223130340_create_friends.rb
+++ db/migrate/20231223130340_create_friends.rb
@@ -1,6 +1,8 @@
 class CreateFriends < ActiveRecord::Migration[7.1]
   def change
     create_table :friends do |t|
+      t.string :name
+      t.string :email
 
       t.timestamps
     end
